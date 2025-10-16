09894a6e44c66beca5994bc42e5ba20c1581aa28
Kim Jiah <2024184004@tukorea.ac.kr>
2025-10-16 20:47:08 +0900

    .

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
 from pico2d import *
-import random
+from boy import Boy
+from grass import Grass
 
 
 # Game object class here
-
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-
-    def update(self):
-        pass
-
-
-class Boy:
-    def __init__(self):
-        self.x, self.y = 400, 90
-        self.frame = 0
-        self.face_dir = 1
-        self.image = load_image('animation_sheet.png')
-
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-
-    def draw(self):
-        if self.face_dir == 1:
-            self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
-        else:
-            self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
 
 
 def handle_events():
@@ -41,6 +15,7 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
 
 
 def reset_world():
@@ -71,6 +46,8 @@ def render_world():
 
 
 running = True
+
+
 
 open_canvas()
 reset_world()
