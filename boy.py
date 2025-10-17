485a740b5f4b1611a53c44c0716e84ac7070d853
Kim Jiah <2024184004@tukorea.ac.kr>
2025-10-17 21:56:35 +0900

    AutoRun도중 좌우 방향 키 입력 시 AutoRun 종료 후 Run

--- boy.py
+++ boy.py
@@ -47,7 +47,7 @@ class Boy:
                 self.SLEEP : {space_down: self.IDLE},
                 self.IDLE : {time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN, auto_run: self.AUTORUN},
                 self.RUN : {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE},
-                self.AUTORUN : {auto_run: self.IDLE}
+                self.AUTORUN : {auto_run: self.IDLE, right_down: self.RUN, left_down: self.RUN}
             }
         )
 
